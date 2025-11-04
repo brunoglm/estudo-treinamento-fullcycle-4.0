fbd7b8243ddd4eac54e17aa14b89a13a5fc837a9
Bruno Gabriel <bruno.glm_@hotmail.com>
2025-11-03 23:22:09 -0300

    add content

---
+++ test-container-lab/golang-lab/redis/01-redis_test.go
@@ -0,0 +1,32 @@
+package redis
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestWithRedis(t *testing.T) {
+	ctx := context.Background()
+	redisC, err := testcontainers.Run(
+		ctx, "redis:latest",
+		testcontainers.WithExposedPorts("6379/tcp"),
+		testcontainers.WithWaitStrategy(
+			wait.ForListeningPort("6379/tcp"),
+			wait.ForLog("Ready to accept connections"),
+		),
+	)
+	testcontainers.CleanupContainer(t, redisC)
+	require.NoError(t, err)
+
+	endpoint, err := redisC.Endpoint(ctx, "")
+	if err != nil {
+		t.Error(err)
+	}
+	assert.NotEmpty(t, endpoint)
+}
